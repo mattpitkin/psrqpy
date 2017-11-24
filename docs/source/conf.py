423ecf23737e76d74d3856724c811de2f3ec19fb
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-11-24 13:58:16 +0000

    conf.py: remove extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinxcontrib.fulltoc'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
