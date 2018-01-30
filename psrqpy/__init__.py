b8aeb0171ab282960950061c2d68fccfd944a395
Matthew Pitkin <matthew.pitkin@ligo.org>
2018-01-30 13:13:37 +0000

    Bumped version number fo include updated requirements file in PyPI release - refs #12

--- psrqpy/__init__.py
+++ psrqpy/__init__.py
@@ -2,7 +2,7 @@
 
 """ A Python tool for interacting with the ATNF pulsar catalogue """
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 
 from .search import QueryATNF
 from .pulsar import Pulsar, Pulsars
