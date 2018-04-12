ad239b163e2cd7494f8129fe0030c83fba499a2d
Matt Pitkin <pitkin@gmail.com>
2018-04-12 11:01:43 +0100

    __init__.py: bump version number

--- psrqpy/__init__.py
+++ psrqpy/__init__.py
@@ -2,7 +2,7 @@
 
 """ A Python tool for interacting with the ATNF pulsar catalogue """
 
-__version__ = "0.4.9"
+__version__ = "0.4.10"
 
 from .search import QueryATNF
 from .pulsar import Pulsar, Pulsars
