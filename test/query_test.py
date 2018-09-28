a5df645d43232d9a10d272fdb219c81b2f6de4ea
Matthew Pitkin <matthew.pitkin@ligo.org>
2018-09-28 17:08:18 +0100

    Start changing the test codes including testing against a couple fo fake catalogues

--- test/query_test.py
+++ test/query_test.py
@@ -1,31 +1,37 @@
-#! /usr/bin/env python
-
 """
-Script to check that all parameters can be queried correctly.
+Test script.
 """
 
+from __future__ import absolute_import
+
+import unittest
 from psrqpy import QueryATNF
 from psrqpy.config import *
 
-# query all parameters for one pulsar (the Crab) one at a time via the webform
-query = QueryATNF(psrs=['J0534+2200'], include_refs=True)
-t = query.table
 
-for i, p in enumerate(PSR_ALL_PARS):
-    try:
-        print('Parameter {}: {}'.format(p, t[p][0]))
-    except KeyError:
-        print('Parameter not in table: {}'.format(p))
+class TestDerived(unittest.TestCase):
+    def setUp(self):
+        self.query_derived = QueryATNF(loadfromdb='test_catalogue.db')
+        self.query_atnf = QueryATNF(loadfromdb='derived_catalogue.db')
 
-    # check error and reference exist if expected
-    if PSR_ALL[p]['err']:
-        try:
-            err = t[p+'_ERR']
-        except IOError:
-            raise IOError('No error value found for parameter "{}"'.format(p))
+    def tearDown(self):
+        del self.query_known
+        del self.query_derived
 
-    if PSR_ALL[p]['ref']:
-        try:
-            ref = t[p+'_REF']
-        except IOError:
-            raise IOError('No reference found for parameter "{}"'.format(p))
+    def test_derived_p0(self):
+        """
+        Test the derived period value against the values from the ATNF Pulsar
+        Catalogue.
+        """
+
+        p0 = self.query_derived.get_pulsar('TEST1')['P0'].value
+        p0atnf = self.query_atnf.get_pulsar('TEST1')['P0'].value
+
+        tol = 1e-9  # tolerance of how close values should be
+
+        self.assertTrue(abs(p0-p0atnf) < tol)
+
+        #p0err =
+
+if __name__ == '__main__':
+    unittest.main()
