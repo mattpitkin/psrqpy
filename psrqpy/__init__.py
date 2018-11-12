73e35c3a31051542588f189424f43c2875d662b6
Matthew Pitkin <matthew.pitkin@ligo.org>
2018-11-12 15:48:25 +0000

    Bump the version number up to 1.0.0

--- psrqpy/__init__.py
+++ psrqpy/__init__.py
@@ -7,7 +7,7 @@ from .search import QueryATNF
 from .pulsar import Pulsar, Pulsars
 from .utils import *
 
-__version__ = "0.6.3"
+__version__ = "1.0.0"
 
 __citation__ = """@article{psrqpy,
   author = {{Pitkin}, M.},
